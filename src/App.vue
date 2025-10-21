1345f574bd66d48aa950aedb3a959e72b76fae34
Alexsandra <alexsandra.meynier@divtec.ch>
2025-10-21 14:31:24 +0200

    ajout d'élément

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app>
     <router-view />
+    <header>
+      <div class="header-container">
+        <div class="app-header">
+          <img alt="Logo du site" class="logo" src="" />
+          <h1>AMIIBO</h1>
+        </div>
+      </div>
+    </header>
   </v-app>
 </template>
 
