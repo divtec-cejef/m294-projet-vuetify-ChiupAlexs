d98e1b4dd0dc7fe8dc2e66a0705025e62f223006
Alexsandra <alexsandra.meynier@divtec.ch>
2025-10-21 14:40:33 +0200

    ajout de titre et de fond sur la page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-    <router-view />
     <header>
       <div class="header-container">
         <div class="app-header">
@@ -9,9 +8,41 @@
         </div>
       </div>
     </header>
+    <main>
+      <h1 class="NosParfums">Bienvenu chez les Amiibo</h1>
+    </main>
   </v-app>
 </template>
 
 <script setup>
   //
 </script>
+<style scoped lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+  color: #000;
+}
+
+header {
+  background: white;
+  padding: 15px 30px;
+  border-bottom: 1px solid #e0e0e0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-container {
+  display: flex;
+}
+
+main {
+  background: #f9f9fb;
+  padding: 30px;
+}
+
+</style>
